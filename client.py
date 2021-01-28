5c6b0562a75a8164fd7837c0fceb62e42977a793
Nazar Mykhailiv <73522074+NazarMykhailiv@users.noreply.github.com>
2021-01-28 09:46:24 +0200

    Update client.py

--- client.py
+++ client.py
@@ -10,9 +10,7 @@ def receving (name, sock):
 		try:
 			while True:
 				data, addr = sock.recvfrom(1024)
-				#print(data.decode("utf-8"))
-
-				# Begin
+				
 				decrypt = ""; k = False
 				for i in data.decode("utf-8"):
 					if i == ":":
